a1a14f632892bcdb1d22486afa7bc65633e1d807
Pedro <pedro.fialho000@gmail.com>
2025-03-27 16:05:50 -0300

    test

--- UaiFood/UaiFood/obj/Debug/net8.0-windows/UaiFood.AssemblyInfo.cs
+++ UaiFood/UaiFood/obj/Debug/net8.0-windows/UaiFood.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UaiFood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947dc64fdee300b57c27fa006421f5344d26db0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e3727c9dc3429f843a7d8196fb8d0c7164849f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UaiFood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UaiFood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
