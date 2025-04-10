0e94e126a64420b95e7e2f658bf3e9c1965c4589
allisson012 <allissoncastilho@gmail.com>
2025-04-10 08:26:49 -0300

    criando a conexão e criando a estrutura do banco

--- UaiFood/UaiFood/BancoDeDados/BancoDados.cs
+++ UaiFood/UaiFood/BancoDeDados/BancoDados.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace UaiFood.BancoDeDados
 {
@@ -13,8 +14,19 @@ namespace UaiFood.BancoDeDados
         private const string bancoDados = "dbUsuarios";
         private const string usuario = "root";
         private const string senha = "1234567";
-
+        private static MySqlConnection connection;
         //declarando variável conexaoBanco para fazer a conexão com o banco de dados
         static public string conexaoBanco = $"server = {servidor}; user id = {usuario}; database= {bancoDados}; password = {senha}";
+        public static void Createconnection()
+        {
+            if (connection == null)
+            {
+                connection = new MySqlConnection(conexaoBanco);
+            }
+        }
+        public void estrutura()
+        {
+
+        }
     }
 }
