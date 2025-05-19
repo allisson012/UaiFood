f34f1175ae393c4fd0eb5b8507cf751a003a61f2
Pedro <pedro.fialho000@gmail.com>
2025-05-19 16:41:48 -0300

    Adicionando telas de edição

---
+++ UaiFood/UaiFood/View/TelaEditarPerfilRestaurante.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace UaiFood.View
+{
+    partial class TelaEditarPerfilRestaurante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picturePerfil = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            txtNome = new TextBox();
+            txtEmail = new TextBox();
+            txtCep = new MaskedTextBox();
+            txtCidade = new TextBox();
+            txtEstado = new TextBox();
+            txtTelefone = new MaskedTextBox();
+            txtRua = new TextBox();
+            txtNumero = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)picturePerfil).BeginInit();
+            SuspendLayout();
+            // 
+            // picturePerfil
+            // 
+            picturePerfil.Image = Properties.Resources.restaurante;
+            picturePerfil.Location = new Point(77, 145);
+            picturePerfil.Margin = new Padding(3, 2, 3, 2);
+            picturePerfil.Name = "picturePerfil";
+            picturePerfil.Size = new Size(191, 239);
+            picturePerfil.SizeMode = PictureBoxSizeMode.Zoom;
+            picturePerfil.TabIndex = 19;
+            picturePerfil.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button1.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(77, 388);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(30, 30);
+            button1.TabIndex = 20;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button2.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(93, 432);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(137, 45);
+            button2.TabIndex = 21;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button3.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(93, 481);
+            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 37);
+            button3.TabIndex = 22;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // txtNome
+            // 
+            txtNome.BackColor = SystemColors.ControlLight;
+            txtNome.BorderStyle = BorderStyle.None;
+            txtNome.Font = new Font("Segoe UI", 12F);
+            txtNome.Location = new Point(324, 186);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(421, 22);
+            txtNome.TabIndex = 23;
+            // 
+            // txtEmail
+            // 
+            txtEmail.BackColor = SystemColors.ControlLight;
+            txtEmail.BorderStyle = BorderStyle.None;
+            txtEmail.Font = new Font("Segoe UI", 12F);
+            txtEmail.Location = new Point(324, 289);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(421, 22);
+            txtEmail.TabIndex = 24;
+            // 
+            // txtCep
+            // 
+            txtCep.BackColor = SystemColors.ControlLight;
+            txtCep.BorderStyle = BorderStyle.None;
+            txtCep.Font = new Font("Microsoft Sans Serif", 12F);
+            txtCep.Location = new Point(324, 399);
+            txtCep.Mask = "00000-000";
+            txtCep.Name = "txtCep";
+            txtCep.Size = new Size(133, 19);
+            txtCep.TabIndex = 25;
+            // 
+            // txtCidade
+            // 
+            txtCidade.BackColor = SystemColors.ControlLight;
+            txtCidade.BorderStyle = BorderStyle.None;
+            txtCidade.Font = new Font("Microsoft Sans Serif", 12F);
+            txtCidade.Location = new Point(324, 488);
+            txtCidade.Name = "txtCidade";
+            txtCidade.Size = new Size(393, 19);
+            txtCidade.TabIndex = 26;
+            // 
+            // txtEstado
+            // 
+            txtEstado.BackColor = SystemColors.ControlLight;
+            txtEstado.BorderStyle = BorderStyle.None;
+            txtEstado.Font = new Font("Microsoft Sans Serif", 12F);
+            txtEstado.Location = new Point(324, 595);
+            txtEstado.Name = "txtEstado";
+            txtEstado.Size = new Size(393, 19);
+            txtEstado.TabIndex = 27;
+            // 
+            // txtTelefone
+            // 
+            txtTelefone.BackColor = SystemColors.ControlLight;
+            txtTelefone.BorderStyle = BorderStyle.None;
+            txtTelefone.Font = new Font("Microsoft Sans Serif", 12F);
+            txtTelefone.Location = new Point(807, 292);
+            txtTelefone.Mask = "(00) 00000-0000";
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Size = new Size(168, 19);
+            txtTelefone.TabIndex = 28;
+            // 
+            // txtRua
+            // 
+            txtRua.BackColor = SystemColors.ControlLight;
+            txtRua.BorderStyle = BorderStyle.None;
+            txtRua.Font = new Font("Microsoft Sans Serif", 12F);
+            txtRua.Location = new Point(807, 399);
+            txtRua.Name = "txtRua";
+            txtRua.Size = new Size(393, 19);
+            txtRua.TabIndex = 29;
+            // 
+            // txtNumero
+            // 
+            txtNumero.BackColor = SystemColors.ControlLight;
+            txtNumero.BorderStyle = BorderStyle.None;
+            txtNumero.Font = new Font("Microsoft Sans Serif", 12F);
+            txtNumero.Location = new Point(807, 488);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(393, 19);
+            txtNumero.TabIndex = 30;
+            // 
+            // TelaEditarPerfilRestaurante
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackgroundImage = Properties.Resources.TelaEditarPerfilRestaurante;
+            ClientSize = new Size(1350, 729);
+            Controls.Add(txtNumero);
+            Controls.Add(txtRua);
+            Controls.Add(txtTelefone);
+            Controls.Add(txtEstado);
+            Controls.Add(txtCidade);
+            Controls.Add(txtCep);
+            Controls.Add(txtEmail);
+            Controls.Add(txtNome);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(picturePerfil);
+            MaximumSize = new Size(1366, 768);
+            MinimumSize = new Size(1366, 768);
+            Name = "TelaEditarPerfilRestaurante";
+            Text = "UaiFood";
+            ((System.ComponentModel.ISupportInitialize)picturePerfil).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picturePerfil;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox txtNome;
+        private TextBox txtEmail;
+        private MaskedTextBox txtCep;
+        private TextBox txtCidade;
+        private TextBox txtEstado;
+        private MaskedTextBox txtTelefone;
+        private TextBox txtRua;
+        private TextBox txtNumero;
+    }
+}
--- UaiFood/UaiFood/View/TelaEditarProduto.Designer.cs
+++ UaiFood/UaiFood/View/TelaEditarProduto.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UaiFood.View
 {
-    partial class TelaAdicionarProduto
+    partial class TelaEditarProduto
     {
         /// <summary>
         /// Required designer variable.
@@ -28,67 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button3 = new Button();
-            button1 = new Button();
-            button2 = new Button();
             pictureBox1 = new PictureBox();
             txtNome = new TextBox();
-            txtDescricao = new TextBox();
             txtPreco = new TextBox();
             label1 = new Label();
             cbCategoria = new ComboBox();
+            txtDescricao = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.Transparent;
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatAppearance.MouseDownBackColor = Color.Transparent;
-            button3.FlatAppearance.MouseOverBackColor = Color.Transparent;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Location = new Point(73, 438);
-            button3.Name = "button3";
-            button3.Size = new Size(186, 43);
-            button3.TabIndex = 12;
-            button3.UseVisualStyleBackColor = false;
-            button3.Click += button3_Click;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.Transparent;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatAppearance.MouseDownBackColor = Color.Transparent;
-            button1.FlatAppearance.MouseOverBackColor = Color.Transparent;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(73, 487);
-            button1.Name = "button1";
-            button1.Size = new Size(115, 47);
-            button1.TabIndex = 13;
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.Transparent;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatAppearance.MouseDownBackColor = Color.Transparent;
-            button2.FlatAppearance.MouseOverBackColor = Color.Transparent;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(73, 389);
-            button2.Name = "button2";
-            button2.Size = new Size(38, 29);
-            button2.TabIndex = 14;
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.comida;
-            pictureBox1.Location = new Point(76, 148);
+            pictureBox1.Location = new Point(76, 147);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(194, 234);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 15;
+            pictureBox1.TabIndex = 16;
             pictureBox1.TabStop = false;
             // 
             // txtNome
@@ -96,42 +55,30 @@
             txtNome.BackColor = SystemColors.ControlLight;
             txtNome.BorderStyle = BorderStyle.None;
             txtNome.Font = new Font("Segoe UI", 16F);
-            txtNome.Location = new Point(373, 190);
+            txtNome.Location = new Point(376, 199);
             txtNome.Name = "txtNome";
             txtNome.Size = new Size(502, 29);
-            txtNome.TabIndex = 17;
-            // 
-            // txtDescricao
-            // 
-            txtDescricao.BackColor = SystemColors.ControlLight;
-            txtDescricao.BorderStyle = BorderStyle.None;
-            txtDescricao.Font = new Font("Segoe UI", 16F);
-            txtDescricao.Location = new Point(373, 507);
-            txtDescricao.Multiline = true;
-            txtDescricao.Name = "txtDescricao";
-            txtDescricao.ScrollBars = ScrollBars.Vertical;
-            txtDescricao.Size = new Size(740, 188);
-            txtDescricao.TabIndex = 19;
+            txtNome.TabIndex = 18;
             // 
             // txtPreco
             // 
             txtPreco.BackColor = SystemColors.ControlLight;
             txtPreco.BorderStyle = BorderStyle.None;
             txtPreco.Font = new Font("Segoe UI", 16F);
-            txtPreco.Location = new Point(373, 299);
+            txtPreco.Location = new Point(376, 301);
             txtPreco.Name = "txtPreco";
             txtPreco.Size = new Size(84, 29);
-            txtPreco.TabIndex = 18;
+            txtPreco.TabIndex = 19;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.White;
             label1.Font = new Font("Segoe UI", 16F);
-            label1.Location = new Point(463, 298);
+            label1.Location = new Point(466, 301);
             label1.Name = "label1";
             label1.Size = new Size(38, 30);
-            label1.TabIndex = 20;
+            label1.TabIndex = 21;
             label1.Text = "R$";
             // 
             // cbCategoria
@@ -140,33 +87,83 @@
             cbCategoria.FlatStyle = FlatStyle.Flat;
             cbCategoria.Font = new Font("Segoe UI", 16F);
             cbCategoria.FormattingEnabled = true;
-            cbCategoria.Location = new Point(373, 408);
+            cbCategoria.Location = new Point(376, 404);
             cbCategoria.Name = "cbCategoria";
             cbCategoria.Size = new Size(502, 38);
-            cbCategoria.TabIndex = 21;
+            cbCategoria.TabIndex = 22;
             // 
-            // TelaAdicionarProduto
+            // txtDescricao
+            // 
+            txtDescricao.BackColor = SystemColors.ControlLight;
+            txtDescricao.BorderStyle = BorderStyle.None;
+            txtDescricao.Font = new Font("Segoe UI", 16F);
+            txtDescricao.Location = new Point(376, 506);
+            txtDescricao.Multiline = true;
+            txtDescricao.Name = "txtDescricao";
+            txtDescricao.ScrollBars = ScrollBars.Vertical;
+            txtDescricao.Size = new Size(740, 188);
+            txtDescricao.TabIndex = 23;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button2.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(76, 387);
+            button2.Name = "button2";
+            button2.Size = new Size(33, 29);
+            button2.TabIndex = 24;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button3.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(76, 438);
+            button3.Name = "button3";
+            button3.Size = new Size(178, 43);
+            button3.TabIndex = 25;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button1.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(76, 487);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 47);
+            button1.TabIndex = 26;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // TelaEditarProduto
             // 
             AutoScaleMode = AutoScaleMode.None;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            BackgroundImage = Properties.Resources.TelaAdicionarProduto;
+            BackgroundImage = Properties.Resources.TelaEditarProduto;
             ClientSize = new Size(1350, 729);
+            Controls.Add(button1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(txtDescricao);
             Controls.Add(cbCategoria);
             Controls.Add(label1);
-            Controls.Add(txtDescricao);
             Controls.Add(txtPreco);
             Controls.Add(txtNome);
             Controls.Add(pictureBox1);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(button3);
             MaximizeBox = false;
             MaximumSize = new Size(1366, 768);
             MinimumSize = new Size(1366, 768);
-            Name = "TelaAdicionarProduto";
+            Name = "TelaEditarProduto";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "UaiFood";
-            Load += TelaAdicionarProduto_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -174,14 +171,14 @@
 
         #endregion
 
-        private Button button3;
-        private Button button1;
-        private Button button2;
         private PictureBox pictureBox1;
         private TextBox txtNome;
-        private TextBox txtDescricao;
         private TextBox txtPreco;
         private Label label1;
         private ComboBox cbCategoria;
+        private TextBox txtDescricao;
+        private Button button2;
+        private Button button3;
+        private Button button1;
     }
 }
